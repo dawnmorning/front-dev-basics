f247a7b6a15514819961c9c5f42b67790522aada
김종혁 <notify9637@naver.com>
2023-10-20 19:31:45 +0900

    dom api #3

--- ch05/src/main/webapp/js/tabBox.js
+++ ch05/src/main/webapp/js/tabBox.js
@@ -1,18 +1,23 @@
 var tabBox = {
 	liSelected: null,
 	_init: function() {
-		var elDiv = document.getElementsByClassName('tab-box')[0]
-		var elUl = elDiv.childNodes[1]
-		var elList = elUl.getElementsByTagName('li')
+		var elDiv = document.getElementsByClassName('tab-box')[0];
+		var elUl = elDiv.childNodes[1];
+		var elLis = elUl.getElementsByTagName('li');
 		
-		Array.prototype.forEach.call(elList, function(li){
-			console.log(li)
-		})
+		Array.prototype.forEach.call(elLis, function(li) {
+			li.onclick = this.onTabClicked.bind(this);
+		}.bind(this));
 	},
 	init: function() {
-		window.onload = this._init
+		window.onload = this._init.bind(this);
 	},
-	onTabClicked: function(li) {
-		console.log(this)
-	}
-}
+	onTabClicked: function(e) {
+		if(this.liSelected != null) {
+			this.liSelected.className = '';
+		}
+	
+		e.target.className = 'selected';
+		this.liSelected = e.target;
+	}	
+};
